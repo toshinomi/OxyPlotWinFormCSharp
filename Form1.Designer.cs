e8df2eda62d76503575af952b005bbc290d1632c
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-08-26 00:26:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace OxyPlotWinFormCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileSelect = new System.Windows.Forms.Button();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.chart = new OxyPlot.WindowsForms.PlotView();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFileSelect
+            // 
+            this.btnFileSelect.BackColor = System.Drawing.Color.White;
+            this.btnFileSelect.Location = new System.Drawing.Point(12, 12);
+            this.btnFileSelect.Name = "btnFileSelect";
+            this.btnFileSelect.Size = new System.Drawing.Size(106, 47);
+            this.btnFileSelect.TabIndex = 0;
+            this.btnFileSelect.Text = "File Select...";
+            this.btnFileSelect.UseVisualStyleBackColor = false;
+            this.btnFileSelect.Click += new System.EventHandler(this.OnClickBtnFileSelect);
+            // 
+            // image
+            // 
+            this.image.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.image.Location = new System.Drawing.Point(12, 76);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(359, 362);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image.TabIndex = 1;
+            this.image.TabStop = false;
+            // 
+            // chart
+            // 
+            this.chart.Location = new System.Drawing.Point(392, 76);
+            this.chart.Name = "chart";
+            this.chart.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.chart.Size = new System.Drawing.Size(502, 362);
+            this.chart.TabIndex = 2;
+            this.chart.Text = "chart";
+            this.chart.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.chart.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.chart.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(906, 450);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.btnFileSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileSelect;
+        private System.Windows.Forms.PictureBox image;
+        private OxyPlot.WindowsForms.PlotView chart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OxyPlotWinFormCSharp
+{
+    public partial class Form1 : Form
+    {
+        private int[] m_nHistgram = new int[256];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart.Model = new OxyPlot.PlotModel { PlotType = OxyPlot.PlotType.XY };
+        }
+
+        public double StandardNormalDistribution(double x)
+        {
+            return 1 / Math.Sqrt(2 * Math.PI) * Math.Exp(-x * x / 2);
+        }
+
+        public void OnClickBtnFileSelect(object sender, EventArgs e)
+        {
+            ComOpenFileDialog openFileDlg = new ComOpenFileDialog();
+            openFileDlg.Filter = "JPG|*.jpg|PNG|*.png";
+            openFileDlg.Title = "Open the file";
+            if (openFileDlg.ShowDialog() == true)
+            {
+                image.Image = null;
+                image.ImageLocation = openFileDlg.FileName;
+
+                var bitmap = (Bitmap)new Bitmap(openFileDlg.FileName).Clone();
+
+                DrawHistgram(bitmap);
+            }
+            return;
+        }
+
+        public void DrawHistgram(Bitmap _bitmap)
+        {
+            InitHistgram();
+
+            CalHistgram(_bitmap);
+
+            var series = new ColumnSeries { FillColor = OxyColors.DarkBlue };
+            for (int nIdx = 0; nIdx < m_nHistgram.Length; nIdx++)
+            {
+                series.Items.Add(new ColumnItem(m_nHistgram[nIdx]));
+            }
+            chart.Model.Series.Clear();
+            chart.Model.Series.Add(series);
+            chart.Model.InvalidatePlot(true);
+        }
+
+        public void CalHistgram(Bitmap _bitmap)
+        {
+            int nWidthSize = _bitmap.Width;
+            int nHeightSize = _bitmap.Height;
+
+            BitmapData bitmapData = _bitmap.LockBits(new Rectangle(0, 0, nWidthSize, nHeightSize), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+            int nIdxWidth;
+            int nIdxHeight;
+
+            unsafe
+            {
+                for (nIdxHeight = 0; nIdxHeight < nHeightSize; nIdxHeight++)
+                {
+                    for (nIdxWidth = 0; nIdxWidth < nWidthSize; nIdxWidth++)
+                    {
+                        byte* pPixel = (byte*)bitmapData.Scan0 + nIdxHeight * bitmapData.Stride + nIdxWidth * 4;
+                        byte nGrayScale = (byte)((pPixel[(int)ComInfo.Pixel.B] + pPixel[(int)ComInfo.Pixel.G] + pPixel[(int)ComInfo.Pixel.R]) / 3);
+
+                        m_nHistgram[nGrayScale] += 1;
+                    }
+                }
+            }
+        }
+
+        public void InitHistgram()
+        {
+            for (int nIdx = 0; nIdx < m_nHistgram.Length; nIdx++)
+            {
+                m_nHistgram[nIdx] = 0;
+            }
+        }
+    }
+}
